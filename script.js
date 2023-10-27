49de590c5ce86242cd23b046a7ab9ddaf04ee725
lucaix <lucasfainerxd@gmail.com>
2023-10-27 00:03:00 -0300

    new niveis

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const niveis = [
     { pergunta: '2 + 2', resposta: 4 },
-    { pergunta: '4 * 5', resposta: 15 },
-    { pergunta: '3 * 5', resposta: 15 },
-    { pergunta: '3 * 5', resposta: 15 },
-    { pergunta: '3 * 5', resposta: 15 },
-    { pergunta: '3 * 5', resposta: 15 },
+    { pergunta: '3 + 5', resposta: 8 },
+    { pergunta: '7 + 3', resposta: 10 },
+    { pergunta: '1 + 9', resposta: 10 },
+    { pergunta: '18 + 4', resposta: 22 },
+    { pergunta: '14 - 9', resposta: 5 },
+    { pergunta: '22 - 14', resposta: 8 },
+    { pergunta: '40 - 27', resposta: 13 },
+    { pergunta: '63 - 48', resposta: 15 },
+    { pergunta: '32 - 18', resposta: 14 },
+    { pergunta: '3 * 4', resposta: 12 },
+    { pergunta: '5 * 7', resposta: 35 },
+    { pergunta: '8 * 6', resposta: 48 },
+    { pergunta: '9 * 5', resposta: 45 },
+    { pergunta: '12 * 2', resposta: 24 },
+    { pergunta: '10 ÷ 2', resposta: 5 },
+    { pergunta: '18 ÷ 3', resposta: 6 },
+    { pergunta: '36 ÷ 6', resposta: 6 },
+    { pergunta: '63 ÷ 9', resposta: 7 },
+    { pergunta: '49 ÷ 7', resposta: 7 }
 ];
 
 let nivelAtual = 0;
